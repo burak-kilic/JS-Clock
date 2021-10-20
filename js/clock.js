51a95be76107a5c08c4c78337acaa1d354cd67b3
Burak Kılıç <kilicburak@protonmail.com>
2021-10-20 22:09:42 +0300

    Added Files.

---
+++ js/clock.js
@@ -0,0 +1,86 @@
+let nameWrite = document.querySelector("#myName");
+name = prompt("Adınızı giriniz: ");
+firstChar = name.slice(0,1);
+firstChar = firstChar.toUpperCase();
+restOf = name.slice(1);
+restOf = restOf.toLowerCase();
+name = firstChar + restOf;
+nameWrite.innerHTML = name;
+let clock = document.querySelector("#myClock");
+
+
+// let month = date.getMonth()+1;
+// let day = date.getDate();
+// let year = date.getFullYear();
+// clock.innerHTML =`${day}.${month}.${year}`;
+let date = new Date();
+let fullDate = date.toDateString();
+let dateArr = fullDate.split(" ");
+let dayString = dateArr[0];
+let month = dateArr[1];
+let dayNumber = dateArr[2];
+let year = dateArr[3];
+
+switch(dayString){
+    case "Mon": 
+        dayString = "Pazartesi";
+        break;
+    case "Tue": 
+        dayString = "Salı";
+        break;
+    case "Wed": 
+        dayString = "Çarşamba";
+        break;
+    case "Thu": 
+        dayString = "Perşembe";
+        break;
+    case "Fri":
+        dayString = "Cuma";
+        break;
+    case "Sat":
+        dayString = "Cumartesi";
+        break;    
+    case "Sun":
+        dayString = "Pazar";
+        break;
+}
+switch(month){
+    case "Jan": 
+        month = "Ocak";
+        break;
+    case "Feb":
+        month = "Şubat";
+        break;
+    case "Mar":
+        month = "Mart";
+        break;
+    case "Apr":
+        month = "Nisan";
+        break;
+    case "May":
+        month = "Mayıs";
+        break;
+    case "Jun":
+        month = "Haziran";
+        break;
+    case "Jul":
+        month = "Temmuz";
+        break;
+    case "Aug":
+        month = "Ağustos";
+        break;
+    case "Sep":
+        month = "Eylül";
+        break;
+    case "Oct":
+        month = "Ekim";
+        break;
+    case "Nov":
+        month = "Kasım";
+        break;
+    case "Dec":
+        month = "Aralık";
+        break;
+}
+
+clock.innerHTML =`${dayNumber} ${month} ${year}, ${dayString}`;
